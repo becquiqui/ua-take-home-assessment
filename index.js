755bf0726a6346dade548e0ce9e8fbda488c48b9
Katlyn Beck <kbeck@catalyte.io>
2020-08-19 12:01:29 -0700

    Add helper function and solidify handleClick function to account for existing rows

--- index.js
+++ index.js
@@ -5,5 +5,24 @@ window.onload = () => {
 };
 
 const handleClick = () => {
-	// Determine what button was clicked and display rows accordingly
+	const rows = document.querySelector('#rows');
+	let cls = event.target.classList.value;
+
+	if (rows.classList.contains('hidden')) {
+		rows.classList.remove('hidden');
+	}
+
+	if (rows.innerHTML) {
+		rows.innerHTML = "";
+		addRows(cls);
+	} else {
+		addRows(cls);
+	}
 }
+
+const addRows = (value) => {
+	let text = `${value.charAt(0).toUpperCase()}${value.slice(1, value.length)}`;
+	for (let i = 0; i < 5; i++) {
+		rows.innerHTML += `<h3>${text}</h3>`;
+	}
+};
