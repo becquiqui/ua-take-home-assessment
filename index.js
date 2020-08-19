e2dde576e34b90d269044038a8221f519ca13c93
Katlyn Beck <kbeck@catalyte.io>
2020-08-19 10:12:53 -0700

    Add event listener and setup function

---
+++ index.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+	document.querySelectorAll('button').forEach(
+		b => b.addEventListener('click', handleClick)
+	);
+};
+
+const handleClick = () => {
+	// Determine what button was clicked and display rows accordingly
+}
